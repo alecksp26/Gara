50c3938d584d771731e90bca1d84e20c25b1a7c1
alecksp26 <76776333+alecksp26@users.noreply.github.com>
2021-05-03 19:08:15 +0200

    Add files via upload

---
+++ automobile.h
@@ -0,0 +1,17 @@
+#include "veicolo.h"
+#include <sstream>
+
+
+class Auto : public Veicolo {
+
+	public:
+
+	Auto(int velocitaMassima, double prezzo, std::string marca, std::string carburante, std::string motorizzazione) : Veicolo(4, velocitaMassima, prezzo, marca, carburante, motorizzazione) {}
+
+	std::string toString() const 
+	{
+		std::stringstream stream;
+		stream << "Sono un'auto di marca " << marca << ", " << motorizzazione << " " << carburante << ". La mia velocita' e' di " << velocita << " km/h";
+		return stream.str();
+	}
+};
