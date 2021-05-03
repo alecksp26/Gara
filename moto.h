a667b61a653773250d885866f81065c1caea64ce
alecksp26 <76776333+alecksp26@users.noreply.github.com>
2021-05-03 19:09:51 +0200

    Add files via upload

---
+++ moto.h
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <sstream>
+#include "veicolo.h"
+
+class Moto : public Veicolo 
+{
+	public:
+		/*
+		int numeroRuote;
+		int velocitaMassima;
+		int velocita;
+		double prezzo;
+		std::string marca;
+		std::string carburante;
+		std::string motorizzazione;
+		*/
+		Moto(int velocitaMassima, double prezzo, std::string marca, std::string motorizzazione) : Veicolo(2, velocitaMassima, prezzo, marca, "benzina", motorizzazione) {}
+
+		std::string toString() const 
+		{
+			std::stringstream stream;
+			stream << "Sono una moto di marca " << marca << ", " << motorizzazione << " " << carburante << ". La mia velocita' e' di " << velocita << " km/h";
+			return stream.str();
+		}
+		/*
+		friend std::ostream& operator<<(std::ostream& out, const Moto& m) {
+			out << m.toString();
+			return out;
+		}
+		*/
+};
